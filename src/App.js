627fb1a22959b9f653699f4a0952e169994ace57
Allan Ramos <allangrds@gmail.com>
2020-12-28 18:27:04 -0300

    style(css): move reset css to another folder

--- src/App.js
+++ src/App.js
@@ -1,25 +1,10 @@
 import React from 'react'
-import logo from './logo.svg'
-import styles from './App.module.css'
+
+import Main from './pages/Main'
 
 function App () {
   return (
-    <div className={styles.App}>
-      <header className={styles['App-header']}>
-        <img src={logo} className={styles['App-logo']} alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className={styles['App-link']}
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Main />
   )
 }
 
---
+++ src/pages/Main/index.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const Main = () => (
+  <p>weee</p>
+)
+
+export default Main
