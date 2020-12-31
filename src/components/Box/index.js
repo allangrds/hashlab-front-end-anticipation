52ef9a318d0be3bb774b0a55b3736be92ca5bf96
Allan Ramos <allangrds@gmail.com>
2020-12-31 13:48:16 -0300

    feat(box): add "className" props

--- src/components/Box/index.js
+++ src/components/Box/index.js
@@ -1,16 +1,22 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import classNames from 'classnames'
 
 import styles from './styles.module.css'
 
-const Box = ({ children }) => (
-  <div className={styles.box}>
+const Box = ({ children, className }) => (
+  <div className={classNames(styles.box, className)}>
     { children }
   </div>
 )
 
 Box.propTypes = {
   children: PropTypes.node.isRequired,
+  className: PropTypes.node,
+}
+
+Box.defaultProps = {
+  className: null,
 }
 
 export default Box
