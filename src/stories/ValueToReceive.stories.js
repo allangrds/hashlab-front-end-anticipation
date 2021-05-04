47e320ea48e4c5b4d209301648c7a42787cd9b02
Allan Ramos <allangrds@gmail.com>
2021-05-04 20:03:51 -0300

    chore(storybook): fix stories path

--- src/stories/ValueToReceive.stories.js
+++ src/stories/ValueToReceive.stories.js
@@ -3,11 +3,16 @@ import ValueToReceive from '../containers/Main/ValueToReceive'
 
 export default {
   component: ValueToReceive,
-  title: 'Example/ValueToReceive',
+  title: 'Containers/Main/ValueToReceive',
 }
 
 const Template = args => <ValueToReceive {...args} />
 
 export const Default = Template.bind({})
 
-Default.args = {}
+Default.args = {
+  in15Days: 11000,
+  in30Days: 12000,
+  in90Days: 13000,
+  tomorrow: 10000,
+}
