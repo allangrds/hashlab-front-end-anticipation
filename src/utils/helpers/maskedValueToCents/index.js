0a7ea6dc86ea2a5192f9b56508d68ed2ea04ec16
Allan Ramos <allangrds@gmail.com>
2021-05-05 12:40:44 -0300

    fix(helpers/maskedvaluetocents): att more validations

--- src/utils/helpers/maskedValueToCents/index.js
+++ src/utils/helpers/maskedValueToCents/index.js
@@ -1,6 +1,10 @@
 import decimalToCents from '../decimalToCents'
 
 function maskedValueToCents (amount) {
+  if (!amount) {
+    return 0
+  }
+
   const brlValue = amount.substr(2)
   const valueWithoutPoint = brlValue.replace(/\./g, '')
   const valueWithPoint = valueWithoutPoint.replace(/,/g, '.')
