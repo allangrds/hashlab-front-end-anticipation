6ae8c7d6fc5bd3518ebec77a381030baf58b6969
Allan Ramos <allangrds@gmail.com>
2021-05-05 02:39:29 -0300

    feat(components/button): add new component

--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,9 @@
 import Box from './Box'
+import Button from './Button'
 import Input from './Input'
 
 export {
   Box,
+  Button,
   Input,
 }
---
+++ src/stories/Button.stories.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Button } from '../components'
+
+export default {
+  component: Button,
+  title: 'Components/Button',
+}
+
+const Template = args => (
+  <Button {...args} />
+)
+
+export const Default = Template.bind({})
+
+Default.args = {
+  onClick: () => alert('Clicou'),
+  text: 'Botão',
+}
