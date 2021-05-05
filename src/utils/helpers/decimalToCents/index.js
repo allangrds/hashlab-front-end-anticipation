5992640ef40c531d21da6d4fd3d21d9f1ed2d790
Allan Ramos <allangrds@gmail.com>
2021-05-05 12:38:34 -0300

    fix(helpers/decimaltocents): att more validations

--- src/utils/helpers/decimalToCents/index.js
+++ src/utils/helpers/decimalToCents/index.js
@@ -1,3 +1,10 @@
-const decimalToCents = amount => parseInt((amount * 100).toFixed(1), 10)
+const decimalToCents = (amount) => {
+  // eslint-disable-next-line no-restricted-globals
+  if (!amount || isNaN(amount)) {
+    return 0
+  }
+
+  return parseInt((amount * 100).toFixed(1), 10)
+}
 
 export default decimalToCents
