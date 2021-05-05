b3471c93b49c5f347c4566e53bbb82410750bc83
Allan Ramos <allangrds@gmail.com>
2021-05-05 18:02:09 -0300

    feat(components/button): add "disabled" prop

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -4,6 +4,7 @@ import PropTypes from 'prop-types'
 import styles from './styles.module.css'
 
 const Button = ({
+  disabled,
   onClick,
   text,
 }) => (
@@ -11,14 +12,20 @@ const Button = ({
     className={styles.button}
     type="button"
     onClick={onClick}
+    disabled={disabled}
   >
     { text }
   </button>
 )
 
 Button.propTypes = {
+  disabled: PropTypes.bool,
   onClick: PropTypes.func.isRequired,
   text: PropTypes.string.isRequired,
+}
+
+Button.defaultProps = {
+  disabled: false,
 }
 
 export default Button
